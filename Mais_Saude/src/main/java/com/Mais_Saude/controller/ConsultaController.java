b77e4790631459b1983b28015450a24d92db4428
erick-vinicius <Aluno@ETEJK-LAB04-M09>
2023-11-21 21:48:39 -0300

    adicionando paciente consulta

--- Mais_Saude/src/main/java/com/Mais_Saude/controller/ConsultaController.java
+++ Mais_Saude/src/main/java/com/Mais_Saude/controller/ConsultaController.java
@@ -1,27 +1,42 @@
 package com.Mais_Saude.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.Mais_Saude.repository.ConsultaRepository;
 import com.Mais_Saude.model.ConsultaModel;
+import com.Mais_Saude.model.PacientesModel;
+import com.Mais_Saude.repository.ConsultaRepository;
+import com.Mais_Saude.repository.PacienteRepository;
+
 
 @Controller
 public class ConsultaController {
 @Autowired
 private ConsultaRepository consultarepository;
+@Autowired
+private PacienteRepository pacienterepository;
+
 	@GetMapping({"/consultas"})
 	public String Consulta(Model model) {
 		model.addAttribute("consulta", consultarepository.findAll());
 	return "/consulta/listar-consultas";
 	}
 
-@GetMapping ("/consultar")
-public String consultar(Model model) {
+@GetMapping ("/consultar-{id}")
+public String consultar(@PathVariable long id, Model model) {
+	Optional<PacientesModel> pacientes = pacienterepository.findById(id);
+	try {
+		model.addAttribute("pacientes", pacientes.get());
+	}
+	catch(Exception err) { return "redirect:/pacientes";}
+	
 	return "/consulta/consultar";
 	}
 
